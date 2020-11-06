d3e31d1449eb2e3f91bb5fc2d1d27a0f27e7a5f0
KingTomi123 <king.tomi@net.hr>
2020-11-06 23:49:07 +0100

    a

---
+++ src/App.vue
@@ -0,0 +1,72 @@
+<template>
+   <div id="app">
+      <div id="container">
+         <div id="tools">
+           <p>Tools:</p>
+            <button>Pen</button>
+            <button>Color</button>
+         </div>
+         <canvas id="canvas" height="400px" width="500px"></canvas>
+         <div id="options">
+          <p>Options:</p>
+            <div v-if="OptPen">
+               
+            </div>
+            <div v-if="OptColor">
+              
+            </div>
+         </div>
+      </div>
+      <br>
+   </div>
+   
+</template>
+
+<script>
+</script>
+
+<style>
+*{
+	margin: 0;
+	padding: 0;
+	box-sizing:border-box;
+	
+}
+#container{
+width:600px;
+height:400px;
+margin:0 auto;	
+
+
+}
+#canvas{
+	border:2px solid black;
+}
+
+#tools{
+	
+	width:504px;
+	height:100px;
+	border:2px solid black;
+  background-color:lightgray;
+}
+button{
+	width:40%;
+	margin-left:6%;
+	height:60px;
+	margin-top:5px;
+  
+}
+#options{
+	width:504px;
+	height:100px;
+	border:2px solid black;
+  margin-top:-4px;
+  background-color:lightgray;
+}
+p{
+  text-align:center;
+  background-color:gray;
+}
+
+</style>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,70 @@
+<template>
+ 
+</template>
+
+<script>
+var app = new Vue({
+		el: '#app',
+		  data: {
+			on: false,
+		  color: "purple",
+		  brightness: 0.5,
+		  
+		  },
+		 computed: {
+		  //computed properties are always FUNCTIONS that RETURNs something
+		  //When the value inide computed is changed it changes the value in html automaticly
+		  style: function(){
+			var styleObject = {
+				backgroundColor: this.color,
+				opacity: this.brightness,
+			}
+			return styleObject;
+		  },
+			
+		   DimPercentage: function(){
+			  return Math.round(this.brightness*100) + "%";
+			
+		  },
+		   
+		   
+		},
+		methods:{
+		  toggle:function(){
+			// this invertes from flase to true and from true to false
+			// <----------- how JS reads this
+		   this.on = !this.on;
+		  },
+		 
+		}
+		});
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.lamp{
+	  position:absolute;
+	  top: 50px;
+	  left 50px;
+	}
+	.shade{
+	  position:aboslute;
+	  width: 70px;
+	  height: 50px;
+	  background-color:bisque;
+	}
+	.light{
+	  position:absolute;
+	  width:100%;
+	  height:100%;
+	  background-color:yellow;
+	}
+	.base{
+	  position: absolute;
+	  width:10px;
+	  height:70px;
+	  left:30px;
+	  top:50px;
+	  background-color:brown;
+	}
+</style>
